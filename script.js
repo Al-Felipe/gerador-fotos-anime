d2f48e6010dcb9db73f6a86830d74e38b1a79f87
Felipe Alves <felipealves3580@gmail.com>
2023-02-07 09:13:02 -0300

    Add click event

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const container = document.querySelector(".container");
 const iconContainer = document.querySelector(".icon-container");
 const imgEl = document.querySelector("img");
 const btnEl = document.getElementById("btn");
+
+//evento
+btnEl.addEventListener("click", generateImage);
