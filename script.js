e218c593b8db5b977fb07dce3f9cafa43dbab46d
Felipe Alves <felipealves3580@gmail.com>
2023-02-07 09:18:41 -0300

    Create generateImage function

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ const btnEl = document.getElementById("btn");
 
 //evento
 btnEl.addEventListener("click", generateImage);
+
+async function generateImage() {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3ee62ab72fmsh5311cd2e221f00dp1fb5b3jsn54be4ca3fe45",
+      "X-RapidAPI-Host": "any-anime.p.rapidapi.com",
+    },
+  };
+  btnEl.innerText = "Carregando...";
+  btnEl.disabled = true;
+  const response = await fetch(
+    "https://any-anime.p.rapidapi.com/anime",
+    options
+  );
+  const data = await response.json();
+  iconContainer.style.display = "block";
+  imgEl.src = data.stuff[0].image;
+  btnEl.innerText = "Gerar Foto";
+  btnEl.disabled = false;
+}
